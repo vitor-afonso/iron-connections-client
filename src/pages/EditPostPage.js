b50c367bd8bba8ad0eb0de4d8c4bd5aec4652bbf
Vitor Afonso <vittokm@hotmail.com>
2022-04-22 23:44:30 +0100

    updated navigation on deleting a post

--- src/pages/EditPostPage.js
+++ src/pages/EditPostPage.js
@@ -1,115 +1,92 @@
 //jshint esversion:9
 
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect } from 'react';
 import { getPost, uploadImage, updatePost, deletePost } from './../api';
 import { AuthContext } from '../context/auth.context';
-import { useParams, useNavigate, NavLink } from "react-router-dom";
+import { useParams, useNavigate, NavLink } from 'react-router-dom';
 
 export const EditPostPage = () => {
+  const { user } = useContext(AuthContext);
+  const [body, setBody] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  let navigate = useNavigate();
 
-    const { user } = useContext(AuthContext);
-    const [body, setBody] = useState("");
-    const [imageUrl, setImageUrl] = useState("");
-    let navigate = useNavigate();
-    
-    const { postId } = useParams();
+  const { postId } = useParams();
 
+  useEffect(() => {
+    (async () => {
+      try {
+        let postFromDB = await getPost(postId);
+        console.log('postFromDB =>', user);
+        setBody(postFromDB.data.body);
+        setImageUrl(postFromDB.data.imageUrl);
+      } catch (error) {
+        console.log('error getting post to update from DB', error);
+      }
+    })();
+  }, [postId, user]);
 
+  const handleFileUpload = async (e) => {
+    try {
+      const uploadData = new FormData();
 
-    useEffect( () => {
+      uploadData.append('imageUrl', e.target.files[0]);
 
-        (async()=>{
+      let response = await uploadImage(uploadData);
 
-            try {
-                
-                let postFromDB = await getPost(postId);
-                console.log('postFromDB =>', user);
-                setBody(postFromDB.data.body);
-                setImageUrl(postFromDB.data.imageUrl); 
+      setImageUrl(response.fileUrl);
+    } catch (error) {
+      console.log('Error while uploading the file: ', error);
+    }
+  };
 
-            } catch (error) {
-                
-                console.log('error getting post to update from DB', error);
-            }
+  const removePost = async () => {
+    try {
+      let response = await deletePost(postId);
+      console.log(response.data.message);
+      navigate(-1);
+    } catch (error) {
+      console.log('Something went wront while deleting post from API', error);
+    }
+  };
 
-        })();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    }, [postId, user]);
+    try {
+      let requestBody = { body, imageUrl };
+      await updatePost(requestBody, postId);
+      navigate(`/feed`);
+    } catch (error) {
+      console.log('Something went wront while updating project in API', error);
+    }
+  };
 
+  return (
+    <div className='AddPost'>
+      {body ? (
+        <form onSubmit={handleSubmit}>
+          {user && <img src={user.imageUrl} alt='Author' style={{ width: '30px' }} />}
 
-    const handleFileUpload = async (e) => {
-        
-        try {
+          <label>
+            <textarea name='body' value={body} onChange={(e) => setBody(e.target.value)} placeholder='Share your thoughts' style={{ minWidth: '300px' }} />
+          </label>
 
-            const uploadData = new FormData();
+          {imageUrl && <img src={imageUrl} alt='Post' style={{ width: '300px' }} />}
 
-            uploadData.append("imageUrl", e.target.files[0]);
-
-            let response = await uploadImage(uploadData);
-            
-            setImageUrl(response.fileUrl);
-
-        } catch (error) {
-
-            console.log("Error while uploading the file: ", error);
-        }
-
-    };
-
-    const removePost = async () => {                    
-
-        try {
-
-            let response = await deletePost(postId);
-            console.log(response.data.message);
-            navigate("/feed");
-
-        } catch (error) {
-
-            console.log('Something went wront while deleting post from API', error);
-        }
-        
-    }; 
-
-    const handleSubmit = async (e) => {
-
-        e.preventDefault();
-
-        try {
-            
-            let requestBody = {body, imageUrl};
-            await updatePost(requestBody, postId);
-            navigate(`/feed`);
-
-        } catch (error) {
-
-            console.log('Something went wront while updating project in API', error);
-        }
-
-    };
-
-    return (
-
-        <div className="AddPost">
-
-            {body ? <form onSubmit={handleSubmit}>    
-            {user && <img src={user.imageUrl} alt="Author" style={{width: "30px"}}/>}
-                
-            <label>
-                <textarea name="body" value={body} onChange={(e) => setBody(e.target.value)} placeholder="Share your thoughts" style={{ minWidth: "300px"}} />
-            </label>
-
-            {imageUrl && <img src={imageUrl} alt="Post" style={{width:"300px"}}/>}
-
-            <label>
-                <input type="file" onChange={(e) => handleFileUpload(e)} />
-            </label>
-            <br/>
-            <button type="submit">Update Post</button> 
-            
-            </form>: <p>Loading...</p>}
-            <NavLink to="/feed"><button onClick={() => navigate(-1)}>Back</button></NavLink>
-            <button onClick={removePost}>Delete</button>
-        </div>
-    )
-}
+          <label>
+            <input type='file' onChange={(e) => handleFileUpload(e)} />
+          </label>
+          <br />
+          <button type='submit'>Update Post</button>
+        </form>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <NavLink to='/feed'>
+        <button onClick={() => navigate(-1)}>Back</button>
+      </NavLink>
+      <button onClick={removePost}>Delete</button>
+    </div>
+  );
+};
